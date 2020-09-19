e402e8284756e12b280873da817d52a97de7a1d7
Milton Micas <69354293+Miltontina@users.noreply.github.com>
2020-09-19 11:51:09 -0700

    Arrays

---
+++ src/mz/co/explicador/correcoes/Exercicio02.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mz.co.explicador.correcoes;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Milton Junior
+ */
+public class Exercicio02 {
+/*     2. Escreva um programa que leia 10 valores reais do teclado e armazene-os num array d e
+ *     permita que sejam impressos quanto solicitados.
+ */
+    public static void main(String[] args) {
+    
+     int totalDeValores= 10;
+     double[] d=new double[totalDeValores];
+     
+     for (int i = 0; i <totalDeValores ; i++){
+          
+         String valores = JOptionPane.showInputDialog("Insira o valor ["+(i+1)+"]");
+     
+     }
+     
+     String solicitacao = JOptionPane.showInputDialog("Deseja solicitar os numeros?: ");
+    
+     String=
+    }
+    
+     
+}
